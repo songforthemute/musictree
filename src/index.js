02505d2769d91975fdbffdf9d4fa6ceab790d2e2
songforthemuto <huni0423@naver.com>
2022-06-19 11:30:50 +0900

    separate css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/footer.css";
+import "./styles/content.css";
+import "./styles/searchResult.css";
+import "./styles/pageControllor.css";
+import "./styles/chart.css";
+import "./styles/search.css";
+import "./styles/homeChart.css";
+import "./styles/loading.css";
+import "./styles/main.css";
+import "./styles/nav.css";
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
